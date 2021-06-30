e8be34cf62f2277facf0c9a839d3493dd1a9a9c7
MrBarskih <ilyabarskih1@gmail.com>
2021-06-30 20:03:58 +0700

    SGTM

--- Assets/Scripts/SafeareaBorder.cs
+++ Assets/Scripts/SafeareaBorder.cs
@@ -1,23 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SafeareaBorder : MonoBehaviour
 {
-    //Calculate the safearea of a phone
+    private GameObject mainCanvas;
+
+    private Dictionary<string, RectTransform> safeAreaElements =
+        new Dictionary<string, RectTransform>();
+
     private void Awake()
     {
-        var rectTransform = gameObject.GetComponent<RectTransform>();
-        var safeArea = Screen.safeArea;
-        var anchorMin = safeArea.position;
-        var anchorMax = anchorMin + safeArea.size;
+        mainCanvas = GameObject.FindGameObjectWithTag("Canvas");
+
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("SafeAreaElement"))
+        {
+            safeAreaElements.Add(item.transform.name, item.GetComponent<RectTransform>());
+        }
+    }
+    private void Start()
+    {
+        safeAreaElements["MainContent"].anchorMax = new Vector2
+            (safeAreaElements["MainContent"].anchorMax.x,
+            1 - (safeAreaElements["Header"].rect.height * mainCanvas.transform.localScale.y / Screen.height));
+
+        CalculateSafeAreaBorders();
+    }
+
+    private void CalculateSafeAreaBorders()
+    {
+        var safeAreaRectTransform = gameObject.GetComponent<RectTransform>();
+        var anchorMin = Screen.safeArea.position;
+        var anchorMax = anchorMin + Screen.safeArea.size;
 
         anchorMin.x /= Screen.width;
         anchorMin.y /= Screen.height;
         anchorMax.x /= Screen.width;
         anchorMax.y /= Screen.height;
 
-        rectTransform.anchorMin = anchorMin;
-        rectTransform.anchorMax = anchorMax;
+        safeAreaRectTransform.anchorMin = anchorMin;
+        safeAreaRectTransform.anchorMax = anchorMax;
     }
-
 }
