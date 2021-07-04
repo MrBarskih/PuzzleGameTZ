92e4aac694996ca27929a8ea5ec08b29a62f83e0
MrBarskih <ilyabarskih1@gmail.com>
2021-07-04 13:03:51 +0700

    SGTM

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -9,6 +9,8 @@ public class LevelBuilder : MonoBehaviour
     public static bool[,] PuzzleTemplate { get; private set; }
     private static IJsonParser jsonLevel;
 
+    [SerializeField] private GameObject tile;
+
     void Awake()
     {
         //singleton
@@ -17,12 +19,13 @@ public class LevelBuilder : MonoBehaviour
         else if (instance == this)
             Destroy(gameObject);
 
-        jsonLevel = Factory.CreateJsonParser("Level1");
+        jsonLevel = Factory.CreateJsonParser("Level2");
     }
 
     private void Start()
     {
         PuzzleTemplate = jsonLevel.GetTemplate();
+        fillPuzzleAreaFromTemplate();
     }
 
     static void Update()
@@ -30,7 +33,16 @@ public class LevelBuilder : MonoBehaviour
         
     }
 
-    private static void fillPuzzleAreaFromTemplate() { 
-    
+    private void fillPuzzleAreaFromTemplate() {
+        var puzzleArea = GameObject.FindGameObjectWithTag("PuzzleArea");
+
+        foreach (bool templateTile in PuzzleTemplate)
+        {
+            var currentTile = Instantiate(tile, puzzleArea.transform);
+            if (templateTile) 
+            {
+                currentTile.AddComponent<TemplateTile>();
+            }
+        }
     }
 }
--- Assets/Scripts/PuzzleArea.cs
+++ Assets/Scripts/PuzzleArea.cs
@@ -5,25 +5,5 @@ using UnityEngine.UI;
 
 public class PuzzleArea : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject tile;
-    private bool[,] template;
-    void Awake()
-    {
-        IJsonParser jsonParser = Factory.CreateJsonParser("Level2");
-        template = jsonParser.GetTemplate();
-    }
-
-    private void Start()
-    {
-        FillPuzzleAreaFromTemplate(template);
-    }
-
-    private void FillPuzzleAreaFromTemplate(bool[,] template) 
-    {
-        foreach(bool i in template)
-        {
-            var currentTile = Instantiate(tile, gameObject.transform).AddComponent<TemplateTile>();
-        }
-    }
+ 
 }
