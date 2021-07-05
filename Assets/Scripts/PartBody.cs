c5a264b9c23392c312493489be3d748e4f0ade24
MrBarskih <ilyabarskih1@gmail.com>
2021-07-05 21:24:33 +0700

    sgtm

--- Assets/Scripts/PartBody.cs
+++ Assets/Scripts/PartBody.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +19,8 @@ public class PartBody : MonoBehaviour
         GoHome();
 
         gameObject.tag = "PartBody";
+
+        
     }
 
     private void Update()
@@ -25,11 +28,33 @@ public class PartBody : MonoBehaviour
         if (Screen.orientation != currentScreenOrientation)
         {
             currentScreenOrientation = Screen.orientation;
+            FitParent();
         }
     }
 
     public void GoHome() 
     {
         partBodyRectTransform.anchoredPosition = new Vector2(0,0);
+    }
+
+    private void FitParent()
+    {
+        var containerSizeX = (gameObject.transform.parent.GetComponent<RectTransform>().anchorMax.x - gameObject.transform.parent.GetComponent<RectTransform>().anchorMin.x) * Screen.safeArea.width;
+        var containerSizeY = 350f;
+
+        var bodyPuzzlePartSizeX = gameObject.GetComponent<RectTransform>().rect.width * GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().localScale.x;
+        var bodyPuzzlePartSizeY = gameObject.GetComponent<RectTransform>().rect.height * GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().localScale.y;
+
+        var containerBodyRatioX = containerSizeX / bodyPuzzlePartSizeX;
+        var containerBodyRatioY = containerSizeY / bodyPuzzlePartSizeY;
+
+        if (containerBodyRatioX > containerBodyRatioY)
+        {
+            gameObject.transform.localScale = new Vector3(containerBodyRatioY, containerBodyRatioY);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(containerBodyRatioX, containerBodyRatioX);
+        }
     }
 }
