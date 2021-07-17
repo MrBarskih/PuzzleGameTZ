042cce24b751891165b7ed10b8c5803bee88ec8a
MrBarskih <ilyabarskih1@gmail.com>
2021-07-17 20:40:56 +0700

    Update PartBodyMagnet.cs

--- Assets/Scripts/GameLogic/PartBodyMagnet.cs
+++ Assets/Scripts/GameLogic/PartBodyMagnet.cs
@@ -7,9 +7,22 @@ public class PartBodyMagnet : MonoBehaviour
     public List<PartTileMagnetAbility> ChildTilesMagnetAbilityComponents = new List<PartTileMagnetAbility>();
 
     private RectTransform rectTransformCompanent;
+    private bool ShouldGoToStartPosition = false;
     private void Awake()
     {
         rectTransformCompanent = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (ShouldGoToStartPosition)
+        {
+            rectTransformCompanent.anchoredPosition = Vector2.MoveTowards(rectTransformCompanent.anchoredPosition, Vector2.zero, 40f);
+            if (rectTransformCompanent.anchoredPosition == new Vector2(0, 0)) 
+            {
+                ShouldGoToStartPosition = false;
+            }
+        }
     }
 
     public void TryToMagnetize() {
@@ -29,7 +42,7 @@ public class PartBodyMagnet : MonoBehaviour
 
     private void ReturnToStartPosition()
     {
-        rectTransformCompanent.anchoredPosition = new Vector2(0, 0);
+        ShouldGoToStartPosition = true;
         rectTransformCompanent.localScale = new Vector2(1, 1);
     }
 }
